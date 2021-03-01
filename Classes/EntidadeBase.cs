36c6d547ff139247d96d6b2dca542ab7e892bb95
Emerson Claudelino <48636325+ebilieri@users.noreply.github.com>
2021-03-01 20:43:34 -0300

    first commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace CadastroMemoria.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace CadastroMemoria.Classes
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        // Métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            // Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Gênero: " + Genero + Environment.NewLine;
+            retorno += "Titulo: " + Titulo + Environment.NewLine;
+            retorno += "Descrição: " + Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + Ano + Environment.NewLine;
+            retorno += "Excluido: " + Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public int retornaId()
+        {
+            return Id;
+        }
+        public bool retornaExcluido()
+        {
+            return Excluido;
+        }
+        public void Excluir()
+        {
+            Excluido = true;
+        }
+    }
+}
